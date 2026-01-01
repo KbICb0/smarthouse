d1c3ecb627974ca24f58df069e8664d3566e2dbc
KbICb0 <108388709+KbICb0@users.noreply.github.com>
2026-01-02 02:22:47 +0300

    поменял драйверо для  подключения к бд

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/KbICb0/smarthouse
 
 go 1.25.5
 
-require github.com/lib/pq v1.10.9
+require github.com/jackc/pgx/v5 v5.8.0
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"os"
 
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	connStr := "user=postgres password=postgres dbname=sensors sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	// Строка подключения
+	connStr := "postgres://postgres:postgres@localhost:5432/sensors?sslmode=disable"
+
+	// Подключение
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Не удалось подключиться: %v\n", err)
+		os.Exit(1)
 	}
-	defer db.Close()
-	err = db.Ping()
+	defer conn.Close(ctx)
+
+	// Проверка
+	err = conn.Ping(ctx)
 	if err != nil {
-		fmt.Printf("Не удалось подключиться к БД: %v\n", err)
+		fmt.Printf("Ошибка ping: %v\n", err)
+		return
 	}
-	fmt.Println("Подключение к БД успешно!")
+
+	fmt.Println("Подключение успешно!")
+
+	rows, err := conn.Query(context.Background(), "select * from rooms")
+	if err != nil {
+		fmt.Printf("Ошибка: %v\n", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var room_name string
+		var temp_lo int
+		var temp_hi int
+
+		err := rows.Scan(&room_name, &temp_lo, &temp_hi)
+		if err != nil {
+			fmt.Printf("Ошибка чтения: %s\n", err)
+			continue
+		}
+		fmt.Printf("Температура в комнате #%s с  %d до %d\n", room_name, temp_lo, temp_hi)
+	}
 }
